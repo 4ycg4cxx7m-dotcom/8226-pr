1cb0a2c6458892ae3e84708d9bfebf513eccc30a
4ycg4cxx7m-dotcom <4ycg4cxx7m@privaterelay.appleid.com>
2026-03-07 00:20:54 +0700

    Format code and remove SSL context from app run

--- 8226.py
+++ 8226.py
@@ -44,5 +44,5 @@ def warmth():
 def inspiration():
     return render_template('inspiration.html')  # Страница побед
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8800, ssl_context='adhoc', debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
